817d60e58b887cab01d1a3d53453f0dfded6e6ae
make it better <levanvung11@gmail.com>
2021-09-29 17:56:09 +0700

    lan2

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
-define("DB_HOST","localhost");
-define("DB_USER","root");
-define("DB_PASS","");
-define("DB_NAME","mvc_web");
+define("DB_HOST","MYSQL5031.site4now.net");
+define("DB_USER","a7a6d1_levanvu");
+define("DB_PASS","vungle2001");
+define("DB_NAME","db_a7a6d1_levanvu");
 ?>
